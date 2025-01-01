508774ec38d5c6d8df221ad2926057ebbfae1876
rafeeq <angadimohammadrafeeq@gamil.com>
2025-01-01 11:26:24 +0530

    provider.tf

--- day-1/main.tf
+++ day-1/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
-  ami = "ami-01816d07b1128cd2d"
-  instance_type = "t2.micro"
-  key_name = "dns"
-  subnet_id = "subnet-0275ce39fea9fc08a"
+  ami = var.ami_id
+  instance_type = var.type
+  key_name = var.keyname
+  subnet_id = var.subnet
 }
--- day-1/provider.tf
+++ day-1/provider.tf
@@ -1,3 +1,12 @@
+# terraform {
+#   required_providers {
+#     hcp = {
+#       source = "hashicorp/hcp"
+#       version = "0.101.0"
+#     }
+#   }
+# }
+
 provider "aws" {
   
 }
