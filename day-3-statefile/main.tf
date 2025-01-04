cfbb455bfb9728b5f9be5f61f13699f7395edcce
rafeeq <angadimohammadrafeeq@gamil.com>
2025-01-04 11:36:20 +0530

    day-3-modified add dynamoDB resource

--- day-3-statefile/main.tf
+++ day-3-statefile/main.tf
@@ -8,3 +8,15 @@ resource "aws_instance" "dev" {
     Name = "test"
   }
 }
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "tterraform-state-lock-dynamodb"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
