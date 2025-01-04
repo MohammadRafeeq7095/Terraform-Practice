1bc08bec185a7e91efc5e61d85975605c13a531b
rafeeq <angadimohammadrafeeq@gamil.com>
2025-01-04 12:40:10 +0530

    backend modified dynamodb

--- day-4-statelocking/backend.tf
+++ day-4-statelocking/backend.tf
@@ -1,7 +1,12 @@
-terraform {
-  backend "s3" {
-    bucket = "terraformtfstatesbucket"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-  }
+
+# Run first terraform-7:30am/day-4-resources_for_backend_s3_dynamodb to create reources 
+# This backend configuration instructs Terraform to store its state in an S3 bucket.
+ terraform {
+backend "s3" {
+    bucket         = "terraformtfstatesbucket"  # Name of the S3 bucket where the state will be stored.
+    region         =  "us-east-1"
+    key            = "terraform.tfstate" # Path within the bucket where the state will be read/written.
+    dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run day-4-bckend resources then day-5-backend config
+    encrypt        = true  # Ensures the state is encrypted at rest in S3.
+}
 }
